e3817ebd61a28cd8b4e4134af1463a042d49041e
Camila13L <scamilalauram@outlook.es>
2024-02-22 10:13:04 -0400

    first and second part of the course done

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -9,20 +9,23 @@ describe('CalculatorService', () => {
         loggerSpy: any;
 
     beforeEach(()=> {
+      console.log("Calling beforeEach");
 
-        console.log("Calling beforeEach");
+      // jasmine.createSpyObj('service in ctor', ["fns"]);
+      //fns: fns that the service use in the class
+      loggerSpy = jasmine.createSpyObj("LoggerService", ["log"]);
 
-        loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
+      //provide dependency and service instead calling ctors explicity
+      TestBed.configureTestingModule({
+        providers: [ //services
+          CalculatorService,
+          //to provide to de before service the injection of the other service
+          { provide: LoggerService, useValue: loggerSpy },
+        ],
+      });
 
-        TestBed.configureTestingModule({
-            providers: [
-                CalculatorService,
-                {provide: LoggerService, useValue: loggerSpy}
-            ]
-        });
-
-        calculator = TestBed.inject(CalculatorService);
-
+      //construct the service we want to
+      calculator = TestBed.inject(CalculatorService);
     });
 
     it('should add two numbers', () => {
@@ -49,5 +52,7 @@ describe('CalculatorService', () => {
         expect(loggerSpy.log).toHaveBeenCalledTimes(1);
 
     });
-
+//unit test != integration test
+// unit test only test the class. Because of that, we inyect the logger service
+// integration test, tests the relation between the two or more clasess
 });
